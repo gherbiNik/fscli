2d1798dfeede0777baef459e6eb9bf8e25c94950
Diego Belloni <diego.belloni@tisctech.com>
2025-10-22 13:10:16 +0200

    Internationalization set up finished!

---
+++ backend/src/main/java/ch/supsi/fscli/backend/util/BackendTranslator.java
@@ -0,0 +1,25 @@
+package ch.supsi.fscli.backend.util;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18nManager {
+    private static final String BUNDLE_BASE_NAME = "i18n.responses";
+
+    public static String getString(String key, Locale locale, Object... args) {
+        try {
+            ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_BASE_NAME, locale);
+            String pattern = bundle.getString(key);
+            
+            if (args.length > 0) {
+                return MessageFormat.format(pattern, args);
+            } else {
+                return pattern;
+            }
+        } catch (Exception e) {
+            System.err.println("Key not found inside the backend bundle: " + key);
+            return "!" + key + "!";
+        }
+    }
+}
---
+++ frontend/src/main/java/ch/supsi/fscli/frontend/util/I18nManager.java
@@ -0,0 +1,4 @@
+package ch.supsi.fscli.frontend.util;
+
+public class I18Manager {
+}

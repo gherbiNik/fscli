0f8cd3b5014ba5c797a35726b2244e7ea99b23d0
Nicholas Gherbi <nicholas.gherbi@gmail.com>
2025-10-24 14:32:15 +0200

    comment

--- frontend/src/main/java/ch/supsi/fscli/frontend/MainFx.java
+++ frontend/src/main/java/ch/supsi/fscli/frontend/MainFx.java
@@ -93,11 +93,15 @@ public class MainFx extends Application {
         MenuItem openMenuItem = new MenuItem(i18n.getString("menu.file.open"));
         openMenuItem.setId("openMenuItem");
 
+        //FIXME: if there is no filesystem created, saving should be disabled:
+        //****************************
         MenuItem saveMenuItem = new MenuItem(i18n.getString("menu.file.save"));
         saveMenuItem.setId("saveMenuItem");
-
+        //saveMenuItem.setDisable(true);
         MenuItem saveAsMenuItem = new MenuItem(i18n.getString("menu.file.save_as"));
         saveAsMenuItem.setId("saveAsMenuItem");
+        //****************************
+
 
         // EXIT MENU
         MenuItem exitMenuItem = new MenuItem(i18n.getString("menu.file.exit"));

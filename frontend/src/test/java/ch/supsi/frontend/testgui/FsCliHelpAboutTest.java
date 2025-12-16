642e97669a625dc716e746803ae895947f9a3bc0
Nicholas Gherbi <nicholas.gherbi@gmail.com>
2025-12-16 11:29:40 +0100

    test gui help and credits v2

--- frontend/src/test/java/ch/supsi/frontend/testgui/FsCliHelpAboutTest.java
+++ frontend/src/test/java/ch/supsi/frontend/testgui/FsCliHelpAboutTest.java
@@ -1,52 +1,91 @@
 package ch.supsi.frontend.testgui;
 
+import ch.supsi.fscli.frontend.util.I18nManager;
+import ch.supsi.fscli.frontend.view.CreditsView;
+import ch.supsi.fscli.frontend.view.HelpView;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.NodeMatchers;
-import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.util.WaitForAsyncUtils;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeoutException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 import static org.testfx.api.FxAssert.verifyThat;
 
+@ExtendWith(ApplicationExtension.class)
+public class FsCliHelpAboutTest {
 
-public class FsCliHelpAboutTest extends AbstractGUITest {
+    @Mock
+    private I18nManager i18nManager;
+
+    private HelpView helpView;
+    private CreditsView creditsView;
+
+    @Start
+    public void start(Stage stage) {
+        MockitoAnnotations.openMocks(this);
+
+        lenient().when(i18nManager.getString(anyString())).thenAnswer(invocation -> "MOCK_" + invocation.getArgument(0));
+    }
+
+    @AfterEach
+    void tearDown() throws TimeoutException {
+        FxToolkit.hideStage();
+    }
 
     @Test
-    public void walkThrough() {
-        testHelp();
-        testAbout();
-    }
-
-    private void testHelp() {
-        step("help...", () -> {
-            // open menu
-            sleep(SLEEP_INTERVAL);
-            clickOn("#helpMenu");
-
-            sleep(SLEEP_INTERVAL);
-            clickOn("#helpMenuItem");
-
-            verifyThat("#helpPopup", NodeMatchers.isVisible());
-            verifyThat("#messageLabel", NodeMatchers.isVisible());
-
-            sleep(SLEEP_INTERVAL);
-            clickOn("#helpPopupOkButton");
+    public void testHelp(FxRobot robot) {
+        Platform.runLater(() -> {
+            helpView = new HelpView(i18nManager);
+            helpView.setCommandDescriptions(Arrays.asList("ls: List files", "cd: Change directory", "mkdir: Create directory"));
+            helpView.show();
         });
+
+        WaitForAsyncUtils.waitForFxEvents();
+
+        verifyThat("#helpPopup", NodeMatchers.isVisible());
+        verifyThat("#messageLabel", NodeMatchers.isVisible());
+
+        verifyThat("#helpPopupOkButton", NodeMatchers.isVisible());
+
+        try { Thread.sleep(1000); } catch (InterruptedException e) {}
+
+        robot.clickOn("#helpPopupOkButton");
     }
 
-    private void testAbout() {
-        step("about...", () -> {
-            // open menu
-            sleep(SLEEP_INTERVAL);
-            clickOn("#helpMenu");
+    @Test
+    public void testAbout(FxRobot robot) {
+        Platform.runLater(() -> {
+            creditsView = new CreditsView(i18nManager);
+            creditsView.setAppName("File System CLI");
+            creditsView.setFrontendVersion("1.0.0");
+            creditsView.setFrontendBuildDate("2025");
+            creditsView.setBackendVersion("2.1.0");
+            creditsView.setBackendBuildDate("2025");
+            creditsView.setAuthorLabel("Studenti SUPSI");
 
-            sleep(SLEEP_INTERVAL);
-            clickOn("#aboutMenuItem");
-
-            verifyThat("#aboutPopup", NodeMatchers.isVisible());
-            verifyThat("#messageLabel", NodeMatchers.isVisible());
-
-            sleep(SLEEP_INTERVAL);
-            clickOn("#aboutPopupOkButton");
+            creditsView.show();
         });
-    }
 
+        WaitForAsyncUtils.waitForFxEvents();
+
+        verifyThat("#aboutPopup", NodeMatchers.isVisible());
+        verifyThat("#messageLabel", NodeMatchers.isVisible());
+
+        try { Thread.sleep(1000); } catch (InterruptedException e) {}
+
+        robot.clickOn("#aboutPopupOkButton");
+    }
 }
